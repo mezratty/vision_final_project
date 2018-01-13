1e75e5854e9dfe032a4e1dc11e8031a08cff5ce8
Stephanie Liu <stephaniel96@gmail.com>
2018-01-13 00:15:37 -0500

    push

--- json_to_mat.py
+++ json_to_mat.py
@@ -10,7 +10,7 @@ NUM_FEATURES = 18 * 2
 NUM_FRAMES = 80
 NUM_CLASSES = 6
 
-def get_movement_data(folder_name, label_val):
+def get_train_movement_data(folder_name, label_val):
     # Get all directories in a folder
     subdirectories = glob.glob(folder_name + "/*/")
     # Iterate through each subdirectory (the various examples of falls and turns)
@@ -20,7 +20,7 @@ def get_movement_data(folder_name, label_val):
         matrix = np.zeros([NUM_FEATURES, NUM_FRAMES])
 
         # For each instance of a fall and turn, get 80 frames and save to a mat file
-        x = (len(subdirectories) - NUM_FRAMES) / 2
+        x = (len(list_files) - NUM_FRAMES) / 2
         for i in range(x, NUM_FRAMES + x):
             # Later will want to edit to choose center 80 frames
             fname = list_files[i]
@@ -39,7 +39,22 @@ def get_movement_data(folder_name, label_val):
 # The way that the data is formatted means that there are no subdirectories for the test data
 # As a result, we get the data in a slightly different way
 def get_test_movement_data(folder_name, label_val):
-    mat_fname = 'mat_test/' + directory[15:-1] + '.mat'
+    list_files = sorted(glob.glob(folder_name) + '/*.json')
+    matrix = np.zeros([NUM_FEATURES, NUM_FRAMES])
+    x = (len(list_files) - NUM_FRAMES) / 2
+    for i in range(x, NUM_FRAMES + x):
+        # Later will want to edit to choose center 80 frames
+        fname = list_files[i]
+        with open(fname) as json_file:
+            json_data = json.load(json_file)
+            keypoints = json_data["people"][0]["pose_keypoints"] # 0 is for first person
+            del keypoints[2::3] # Check how to make this robust later
+            matrix[:, i] = keypoints
+    
+    mat_fname = 'mat_test/' + folder_name[15:-1] + '.mat'
+    label = np.zeros(NUM_CLASSES)
+    label[label_val - 1] = 1
+    sio.savemat(mat_fname, mdict = {'keypoints': matrix, 'label': label})
 
 
 # Label code
@@ -47,71 +62,71 @@ def get_test_movement_data(folder_name, label_val):
 # Jump: 2 (jump to stage right) and 5 (jump to stage left)
 # Fall: 3 (to stage right) and 6 (fall to stage left)
 def get_maia_turn():
-    get_movement_data('maia_turn', 1)
+    get_train_movement_data('maia_turn', 1)
 
 def get_maia_jump():
-    get_movement_data('maia_jump', 2)
+    get_train_movement_data('maia_jump', 2)
 
 def get_maia_fall():
-    get_movement_data('maia_fall', 3)
+    get_train_movement_data('maia_fall', 3)
 
 def get_sliu_turn():
-    get_movement_data('sliu_turn', 4)
+    get_train_movement_data('sliu_turn', 4)
 
 def get_sliu_jump():
-    get_movement_data('sliu_jump', 5)
+    get_train_movement_data('sliu_jump', 5)
 
 def get_sliu_fall():
-    get_movement_data('sliu_fall', 6)
+    get_train_movement_data('sliu_fall', 6)
 
 ################### TESTING DATA ####################
 def test_1():
-    get_movement_data('test_keypoints/test1_maia_turn', 1)
+    get_test_movement_data('test_keypoints/test1_maia_turn', 1)
 
 def test_2():
-    get_movement_data('test_keypoints/test2_maia_jump', 5)
+    get_test_movement_data('test_keypoints/test2_maia_jump', 5)
 
 def test_3():
-    get_movement_data('test_keypoints/test3_maia_jump', 5)
+    get_test_movement_data('test_keypoints/test3_maia_jump', 5)
 
 def test_4():
-    get_movement_data('test_keypoints/test4_maia_turn', 1)
+    get_test_movement_data('test_keypoints/test4_maia_turn', 1)
 
 def test_5():
-    get_movement_data('test_keypoints/test5_maia_fall', 3)
+    get_test_movement_data('test_keypoints/test5_maia_fall', 3)
 
 def test_6():
-    get_movement_data('test_keypoints/test6_maia_jump', 2)
+    get_test_movement_data('test_keypoints/test6_maia_jump', 2)
 
 def test_7():
-    get_movement_data('test_keypoints/test7_maia_fall', 5)
+    get_test_movement_data('test_keypoints/test7_maia_fall', 5)
 
 def test_8():
-    get_movement_data('test_keypoints/test8_maia_double', 1)
+    get_test_movement_data('test_keypoints/test8_maia_double', 1)
 
 def test_9():
-    get_movement_data('test_keypoints/test9_both_turn', 1)
+    get_test_movement_data('test_keypoints/test9_both_turn', 1)
 
 def test_10():
-    get_movement_data('test_keypoints/test10_both_turn', 1)
+    get_test_movement_data('test_keypoints/test10_both_turn', 1)
 
 def test_11():
-    get_movement_data('test_keypoints/test11_both_jump', 2)
+    get_test_movement_data('test_keypoints/test11_both_jump', 2)
 
 def test_12():
-    get_movement_data('test_keypoints/test12_maia_up', 6)
+    get_test_movement_data('test_keypoints/test12_maia_up', 6)
 
 def test_13():
-    get_movement_data('test_keypoints/test13_maia_occ', 2)
+    get_test_movement_data('test_keypoints/test13_maia_occ', 2)
 
 def test_14():
-    get_movement_data('test_keypoints/test14_maia_jump_close', 2)
+    get_test_movement_data('test_keypoints/test14_maia_jump_close', 2)
 
 def test_15():
-    get_movement_data('test_keypoints/test15_maia_fall_close', 3)
+    get_test_movement_data('test_keypoints/test15_maia_fall_close', 3)
 
 def test_16():
-    get_movement_data('test_keypoints/test16_maia_turn_close', 1)
+    get_test_movement_data('test_keypoints/test16_maia_turn_close', 1)
 
 def main():
     # get_maia_jump()
@@ -133,6 +148,11 @@ def main():
     test_9()
     test_10()
     test_11()
+    test_12()
+    test_13()
+    test_14()
+    test_15()
+    test_16()
 
 if __name__ == "__main__":
     main()
